caa7427e7561b5b9e052b84855d887da1c98dcdc
EddyPBR <edvaldojunior1310@gmail.com>
2022-12-15 00:16:37 -0300

    :sparkles: finishing class three

---
+++ src/application/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,27 @@
+import { Notification } from "@application/entities/notification";
+import { Injectable } from "@nestjs/common";
+import { NotificationsRepository } from "../repositories/notification-repository";
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationsResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationsRequest,
+  ): Promise<GetRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationRepository.findManyByRecipientId(recipientId);
+
+    return { notifications };
+  }
+}
--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { randomUUID } from "node:crypto";
 import { CreateNotificationBody } from "./http/dtos/create-notification-body";
-import { PrismaService } from "./database/prisma.service";
+import { PrismaService } from "./database/prisma/prisma.service";
 
 @Controller("notifications")
 export class AppController {
